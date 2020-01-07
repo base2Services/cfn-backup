33f6d96187277aa60efd733026efe8b25193cd23
lohgannash <lohgannash@gmail.com>
2020-01-07 11:41:41 +1100

    Additional code commenting missed for publishing

--- lib/cfnbackup/publish.rb
+++ lib/cfnbackup/publish.rb
@@ -28,6 +28,7 @@ module CfnBackup
       Log.logger.debug("Log level set to DEBUG")
     end
 
+    # Sets the stack name to be used in template name & resource names. Defaults to cfnbackup if none provided
     def set_stack_name
       if @options['stack_name']
         @stack_name = @options['stack_name']
@@ -38,6 +39,7 @@ module CfnBackup
       end
     end
 
+    # Enforces source bucket to be provided and sets it if it is
     def set_source_bucket
       if !@options['source_bucket']
         Log.logger.debug("No source bucket provided")
@@ -48,6 +50,7 @@ module CfnBackup
       end
     end
 
+    # Creates the build dir based on the stack name
     def create_build_directory
       @build_dir = "output/#{@stack_name}"
       Log.logger.debug("Creating output directory #{@build_dir}")
@@ -56,32 +59,41 @@ module CfnBackup
 
     def initialize_config
       Log.logger.debug("Initialising config, loading global config file")
+      # Load the global config file (should always be present in the hardcoded path)
       global_config_path = File.join(File.dirname(__FILE__), '../config/global_config.yml')
       global_config = YAML.load(File.read(global_config_path))
+      # Check if a custom config file has been provided with the --config flag
       if @options['config']
         Log.logger.debug("Custom config file path provided, attempting to load")
+        # Check if the file/path provided is a valid file and attempt to load it using the YAML object.
         if File.file?(@options['config'])
           custom_config = YAML.load(File.read(@options['config']))
           Log.logger.debug("Custom config file loaded, deep merging with global config")
+          # Peform a deep merge on the loaded global config and the custom config
           @config = CfnBackup::Utils.deep_merge(global_config, custom_config)
         else
           abort("Could not find or load file #{@options['config']}")
         end
       else
+        # If no custom config was provided no further action is needed
         Log.logger.debug("No custom config file provided, using all default values")     
         @config = global_config
       end
+      # Load the stack name and source bucket taken from ARGS/Defaults and insert into the final config
       @config['stack_name'] = @stack_name
       @config['source_bucket'] = @source_bucket
     end
 
     def publish_cloudformation
       Log.logger.debug("Populating CfHighlander file from template")
+      # Inject the initalised config list into the text template which will use these to populate parameters
       template('templates/cfnbackup.cfhighlander.rb.tt', "#{@build_dir}/#{@stack_name}.cfhighlander.rb", @config, force: true)
       Log.logger.debug("Generating CloudFormation template from #{@build_dir}/#{@stack_name}.cfhighlander.rb")
+      # Initalise the CfHighlander object and run a render, this will compile and validate the component, outputting cloudformation
       cfhl = CfnBackup::CfHighlander.new(@options['region'], @stack_name, nil, @build_dir)
       compiler = cfhl.render()
       Log.logger.debug("CloudFormation template generated and validated")
+      # Publishes the compiled cloudformation to S3 using the source bucket provided, outputting the master stack S3 path
       @template_url = cfhl.publish(compiler)
     end
 
